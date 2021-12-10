2e7280c931b19b06c63036a5932385d7a2c6a95d
bruno-bergamasco <bruno.bergamasco@ibm.com>
2021-12-10 15:19:12 -0300

    first commit

---
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,31 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringHelperTest {
+
+        StringHelper stringHelper = new StringHelper();
+
+        @Test
+        public void testTruncateAInFirst2Positions_Ainfirst2Positions(){
+            assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
+        }
+
+        @Test
+        public void testTruncateAInFirst2Positions_AinfirstPosition(){
+            assertEquals("CDA", stringHelper.truncateAInFirst2Positions("ACDA"));
+        }
+
+        @Test
+        public void testAreFirstAndLastTwoCharactersTheSame_False(){
+            assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+        }
+
+        @Test
+        public void testAreFirstAndLastTwoCharactersTheSame_True(){
+            assertTrue("A condicao falhou",stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+        }
+
+}
